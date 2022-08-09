e65a3ae6f9c7f5cdbffecbdf9109c646f6d4fd4d
André Vinicius Bezerra da Silva <105171947+AVBDS001@users.noreply.github.com>
2022-08-09 11:36:16 -0300

    Create soma100.cpp

---
+++ soma100.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	
+	int sum = 0;
+	for(int i = 0; i <= 100; i++) {
+		sum += i;
+	}
+	cout << sum;
+	
+	return 0;
+}
