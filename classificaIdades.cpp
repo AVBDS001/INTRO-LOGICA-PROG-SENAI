f5867d143a867038178213db2b4a1499b875f188
André Vinicius Bezerra da Silva <105171947+AVBDS001@users.noreply.github.com>
2022-09-26 08:13:23 -0300

    Create classificaIdades.cpp

---
+++ classificaIdades.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int idade;
+void classificar(int idade) {
+	if (idade >= 1 && idade <= 5) {
+		cout<<"Infantil"<<endl;
+	}
+	else if (idade >= 6 && idade <= 10) {
+		cout<<"Juvenil A"<<endl;
+	}
+	else if (idade >= 11 && idade <= 15) {
+		cout<<"Juvenil B"<<endl;
+	}
+	else if (idade >= 16 && idade <= 17) {
+		cout<<"Jovem"<<endl;
+	}
+	else if (idade >= 18 && idade <= 30) {
+		cout<<"Adulto"<<endl;
+	}
+	else if (idade > 30) {
+		cout<<"Master"<<endl;
+	}
+}
+
+int main() {
+	cout<<"Insira a sua idade"<<endl;
+	cin>>idade;
+	while (idade <= 0) {
+		cout<<"VALOR INVALIDO DIGITE NOVAMENTE"<<endl;
+		cin>>idade;
+	}
+	char resposta;
+	while (resposta != 'S') {
+		classificar(idade);
+		cout<<"DESEJA SAIR DO PROGRAMA (Digite --> S para sair/Digite --> N para ficar): "<<endl;
+		cin>>resposta;	
+	}
+}
