bde2e5adfbf0448e7a0a1922c2dd0fee9ee8c0fb
André Vinicius Bezerra da Silva <105171947+AVBDS001@users.noreply.github.com>
2022-10-03 11:04:14 -0300

    Create Cardapio.java

---
+++ src/entities/Cardapio.java
@@ -0,0 +1,15 @@
+
+package entities;
+
+public class Cardapio {
+    
+    public Cardapio() {
+    }
+    
+    public void menu() {
+        System.out.println("Feijoada: R$ 15.00");
+        System.out.println("Frango assado: R$ 15.00");
+        System.out.println("Estrogonofe de camarão: R$ 20.00");
+        System.out.println("PF de churrasco: R$ 20.00");
+    }
+}
