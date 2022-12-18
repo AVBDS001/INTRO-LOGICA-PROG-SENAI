950134cad54dbf38346c7b90652f7281d4e00a10
André Vinicius Bezerra da Silva <105171947+AVBDS001@users.noreply.github.com>
2022-12-18 10:14:23 -0300

    Update projetoFinal.cpp

--- projetoFinal.cpp
+++ projetoFinal.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<cstdlib>
+#include<stdlib.h>
 using namespace std;
 
 void menuPrincipal() {
@@ -31,9 +33,13 @@ void menuRelatorios() {
 	cout<<"1-Exibir a maior venda"<<endl;
 	cout<<"2-Exibir o total de todas as vendas"<<endl;
 	cout<<"3-Exibir a venda de um cliente especifico"<<endl;
-	cout<<"4-Exibir todas as vendas de um funcionario"<<endl;
-    cout<<"5-Voltar para o menu principal"<<endl;
+	cout<<"4-Voltar para o menu principal"<<endl;
 }
+
+double maiorValor = 0.0;
+int idDaMaiorVenda = 0;
+double soma = 0.0;
+
 
 struct Cliente {
 	int id;
@@ -41,6 +47,7 @@ struct Cliente {
 	string telefone;
 	string cpf;
 };
+int idClienteEscolhido = 0;
 
 struct Funcionario {
 	int id;
@@ -49,12 +56,16 @@ struct Funcionario {
 	string cpf;
 };
 
+int idFuncionarioEscolhido = 0;
+
 struct Produto {
 	int id;
 	string nome;
 	int qtdProdutos;
 	double preco;	
 };
+int idProdutoEscolhido = 0;
+int qtdAtualDoProduto = 0;
 
 struct Venda {
     int id;
@@ -68,19 +79,25 @@ struct Venda {
 int clientesSize = 0;
 int funcionariosSize = 0;
 int produtosSize = 0;
+int vendasSize = 0;
 Cliente clientes[999];
 Funcionario funcionarios[999];
 Produto produtos[999];
 Venda vendas[999];
 
+int gerarID() {
+    int N = 99999;
+    for (int i = 0; i < 1; i++) {
+        return rand() % N;
+    }
+}
 //Metodos dos clientes
 void cadastrarCliente() {
     for (int i = 0; i < 999; i++) {
         if(clientes[i].id == 0) {
-            cout<<"Informe o ID do cliente "<<i+1<<" : ";
-            cin>>clientes[i].id;
+            clientes[i].id = gerarID();
+            cout<<"ID DO CLIENTE "<<i+1<<" : "<<clientes[i].id;
             cout<<"\n";
-
             cout<<"Informe o nome do cliente "<<i+1<<" : ";
             cin>>clientes[i].nome;
             cout<<"\n";
@@ -88,7 +105,6 @@ void cadastrarCliente() {
             cout<<"Informe o telefone do cliente "<<i+1<<" : ";
             cin>>clientes[i].telefone;
             cout<<"\n";
-
             cout<<"Informe o CPF do cliente "<<i+1<<" : ";
             cin>>clientes[i].cpf;
             cout<<"\n";
@@ -112,10 +128,9 @@ void exibirClientesCadastrados() {
 void cadastrarFuncionario() {
     for (int i = 0; i < 999; i++) {
         if (funcionarios[i].id == 0) {
-            cout<<"Informe o ID do funcionario "<<i+1<<" : ";
-            cin>>funcionarios[i].id;
+            funcionarios[i].id = gerarID();
+            cout<<"ID DO FUNCINARIO "<<i+1<<" : "<<funcionarios[i].id;
             cout<<"\n";
-
             cout<<"Informe o nome do funcionario "<<i+1<<" : ";
             cin>>funcionarios[i].nome;
             cout<<"\n";
@@ -123,7 +138,6 @@ void cadastrarFuncionario() {
             cout<<"Informe o telefone do funcionario "<<i+1<<" : ";
             cin>>funcionarios[i].telefone;
             cout<<"\n";
-
             cout<<"Informe o CPF do funcionario "<<i+1<<" : ";
             cin>>funcionarios[i].cpf;
             cout<<"\n";
@@ -147,15 +161,14 @@ void exibirFuncionariosCadastrados() {
 void cadastrarProduto() {
     for (int i = 0; i < 999; i++) {
         if (produtos[i].id == 0) {
-            cout<<"Informe o ID do produto: "<<i+1<< " : ";
-            cin>>produtos[i].id;
+            produtos[i].id = gerarID();
+            cout<<"ID DO PRODUTO "<<i+1<<" : "<<produtos[i].id;
             cout<<"\n";
-
             cout<<"Informe o nome do produto: "<<i+1<<" : ";
             cin>>produtos[i].nome;
             cout<<"\n";
 
-            cout<<"Informe quantos produtos "<<i+1<<" serao comprados  : ";
+            cout<<"Informe quantos produtos "<<i+1<<" serao adicionados no estoque: ";
             cin>>produtos[i].qtdProdutos;
             cout<<"\n";
 
@@ -178,9 +191,16 @@ void exibirProdutosCadastrados() {
     }
 }
 
+//METODOS DA VENDAS
+double subtotal = 0.0;
+
+
 int main() {
+    //NA HORA DE TESTAR O CODIGO RECOMENDO ANOTAR OS CODIGOS PARA NAO ESQUECER
+    //MENU PRINCIPAL
     int opPrincipal = 0;
     while (opPrincipal != 4) {
+        system("clear");
         menuPrincipal();
         cout<<"Insira uma opcao do menu: ";
         cin>>opPrincipal;
@@ -192,6 +212,7 @@ int main() {
             char addFuncionario = 'N';
             char addProduto = 'N';
             while(opCadastro != 4) {
+                //MENU CADASTRO
                 menuCadastro();
                 cout<<"Insira uma opcao do menu: ";
                 cin>>opCadastro;
@@ -228,7 +249,10 @@ int main() {
             }
         }
         else if(opPrincipal == 2) {
+            //MENU DE VENDAS
             int opVendas = 0;
+            //int idProdutoEscolhido = 0;
+            int quantidadeParaComprar = 0;
             if (clientes[0].id == 0 && produtos[0].id == 0 && funcionarios[0].id == 0) {
                     cout<<"ERRO: NECESSARIO CADASTRAR OS DADOS DO CLIENTE, DOS FUNCIONARIOS, E DOS PRODUTOS PARA REALIZAR VENDAS"<<endl;
                     cout<<"PRESSIONE 5 PARA RETORNAR AO MENU PRINCIPAL: ";
@@ -236,11 +260,12 @@ int main() {
                     cout<<"\n";
                 }
             while (opVendas != 5) {
+                //system("clear");
                 menuVendas();
+                cout<<"RECOMENDACAO: UTILIZE AS OPCOES DE EXIBIR PRODUTOS ANTES DE REALIZAR UMA COMPRA"<<endl;
                 cout<<"Insira uma opcao do menu: ";
                 cin>>opVendas;
                 cout<<"\n";
-
                 switch(opVendas) {
                     case 1:
                         exibirClientesCadastrados();
@@ -255,31 +280,120 @@ int main() {
                         break;
 
                     case 4:
-                        //realizacao da compra
+                        //REALIZAÇÃO DA COMPRA
+                        cout<<"Insira o ID do produto a ser comprado: ";
+                        cin>>idProdutoEscolhido;
+                        cout<<"\n";
+
+                        cout<<"Insira o ID do cliente que solicitou a compra: ";
+                        cin>>idClienteEscolhido;
+                        cout<<"\n";
+
+                        for (int i = 0; i < clientesSize; i++) {
+                            if (idClienteEscolhido == clientes[i].id) {
+                                cout<<"CLIENTE: "<<clientes[i].nome<<" SELECIONADO "<<endl;
+                            }
+                        }
+
+                        for (int i = 0; i < produtosSize; i++) {
+                            if (idProdutoEscolhido == produtos[i].id) {
+                                cout<<"PRODUTO: "<<produtos[i].nome<<" SELECIONADO "<<endl;
+                                cout<<"Insira a quantidade de produtos que sera comprada: ";
+                                cin>>quantidadeParaComprar;
+                                cout<<"\n";
+                                //remoção do produto do estoque
+                                qtdAtualDoProduto = produtos[i].qtdProdutos - quantidadeParaComprar;
+                                if (quantidadeParaComprar >= produtos[i].qtdProdutos) {
+                                    cout<<"NAO TEMOS ESSA QUANTIDADE NO ESTOQUE NO MOMENTO !!!"<<endl;
+                                }
+                                vendas[i].id = gerarID();
+                                cout<<"ID DE IDENTIFICACAO DA VENDA: "<<vendas[i].id<<endl;
+                                cout<<"ID DO CLIENTE QUE REQUISITOU A VENDA: "<<clientes[i].id<<endl;
+                                cout<<"QUANTIDADE ATUAL DO PRODUTO NO ESTOQUE: "<<qtdAtualDoProduto<<endl;
+                                subtotal = produtos[i].preco * quantidadeParaComprar; 
+                                cout<<"VALOR DA VENDA: $ "<<subtotal;
+                                cout<<"\n";
+                                cout<<"\n";
+                                vendasSize++;
+                            }
+                        }
                         break;
                 }
             }
         }
 
         else if (opPrincipal == 3) {
-            int opRelatorios = 0;
+            //MENU DE RELATORIOS
+            int opMenuRelatorios = 0;
+            int codigoDaVenda = 0;
             if (clientes[0].id == 0 && produtos[0].id == 0 && funcionarios[0].id == 0) {
                     cout<<"ERRO: NECESSARIO CADASTRAR OS DADOS DO CLIENTE, DOS FUNCIONARIOS, E DOS PRODUTOS PARA CHECAR OS RELATORIOS"<<endl;
                     cout<<"PRESSIONE 5 PARA RETORNAR AO MENU PRINCIPAL: ";
-                    cin>>opRelatorios;
+                    cin>>opMenuRelatorios;
                     cout<<"\n";
                 }
-            while (opRelatorios != 5) {
+            while (opMenuRelatorios != 4) {
                 menuRelatorios();
                 cout<<"Insira uma opcao: ";
-                cin>>opRelatorios;
+                cin>>opMenuRelatorios;
                 cout<<"\n";
 
-                //fazer o switch case
+                switch(opMenuRelatorios) {
+                    case 1:
+                        //system("clear");
+                        //double maiorValor = 0.0;
+                        //int idDaMaiorVenda = 0;
+
+                        for (int i = 0; i < vendasSize; i++) {
+                            if (maiorValor < subtotal) {
+                                maiorValor = subtotal;
+                                idDaMaiorVenda = vendas[i].id;
+                            }
+                        }
+                        cout<<"MAIOR VENDA $: "<<maiorValor<<endl;
+                        cout<<"ID DA MAIOR VENDA: "<<idDaMaiorVenda<<endl;
+                        cout<<"\n";
+                        break;
+
+                    case 2:
+                        //double soma = 0.0;
+                        for (int i = 0; i < vendasSize; i++) {
+                            soma += subtotal;
+                        }
+                        cout<<"VALOR TOTAL DE VENDAS: $ "<<soma<<endl;
+                        cout<<"\n";
+                        break;
+
+                    case 3:
+                        exibirClientesCadastrados();
+                        cout<<"Escolha o ID do cliente que voce quer averiguar a venda: ";
+                        cin>>idClienteEscolhido;
+                        cout<<"\n";
+
+                        for (int i = 0; i < clientesSize; i++) {
+                            if (idClienteEscolhido == clientes[i].id) {
+                                cout<<"Insira o codigo de venda que foi gerado na venda desse cliente: ";
+                                cin>>codigoDaVenda;
+                                cout<<"\n";
+
+                                for (int i = 0; i < vendasSize; i++) {
+                                    if (codigoDaVenda == vendas[i].id) {
+                                        cout<<"ID DE IDENTIFICACAO DA VENDA: "<<vendas[i].id<<endl;
+                                        cout<<"ID DO CLIENTE QUE REQUISITOU A VENDA: "<<clientes[i].id<<endl;
+                                        cout<<"QUANTIDADE ATUAL DO PRODUTO NO ESTOQUE: "<<qtdAtualDoProduto<<endl;
+                                        cout<<"VALOR DA VENDA: $ "<<subtotal<<endl;
+                                        cout<<"NOME DO CLIENTE: "<<clientes[i].nome<<endl;
+                                        cout<<"NOME DO FUNCIONARIO: "<<funcionarios[i].nome<<endl;
+                                        cout<<"\n";
+                                        cout<<"\n";
+                                    }
+                                }
+                            }
+                        }
+                        break;    
+                }
             }
         }
     }
-
-
    return 0; 
 }
